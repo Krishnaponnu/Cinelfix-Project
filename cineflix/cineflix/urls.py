32f8e09f0e7601c11b02ee020fb5d3e3d5b09be1
krishna <krishnarajeevlakshmy@gmail.com>
2025-12-23 12:08:51 +0530

    subscriptions

--- cineflix/cineflix/urls.py
+++ cineflix/cineflix/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 from django.conf.urls.static import static
 
@@ -23,6 +23,13 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('',include('movies.urls')),
+
+    path('',include('authentication.urls')),
+
+    path('',include('subscriptions.urls')),
+
 ]
 
 
